dead39f279ebf6a668e544806110a01910160897
EraserandRain <1349291258@qq.com>
2022-03-01 13:33:54 +0800

    perf: optimize build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export LC_ALL=C
+rm -rf dist/*
+rm -rf docs/*
 npx parcel build src/index.html
 /usr/bin/cp -r dist/* docs/
+css=$(ls -l docs|grep "css$"|awk '{print $NF}')
+js=$(ls -l docs|grep "js$"|awk '{print $NF}')
+sed -i "s|/${css}|/docs/${css}|" ./docs/index.html
+sed -i "s|/${js}|/docs/${js}|" ./docs/index.html
 exit 0
