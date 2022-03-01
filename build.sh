b459071c6f12861d61d8d2f1ca4eb08aa3484295
EraserandRain <1349291258@qq.com>
2022-03-02 01:36:30 +0800

    feat: update html project section

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 export LC_ALL=C
 rm -rf dist/*
 rm -rf docs/*
-npx parcel build src/index.html
+npx parcel build src/index.html --no-minify
 /usr/bin/cp -r dist/* docs/
 css=$(ls -l docs|grep "css$"|awk '{print $NF}')
 js=$(ls -l docs|grep "js$"|awk '{print $NF}')
--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
-var myChart = echarts.init(document.getElementById('main'));
+var myChart = echarts.init(document.getElementById('skills'));
 var option = {
-    title: {
-        text: 'Basic Radar Chart'
-    },
-    legend: {
-        data: ['Allocated Budget', 'Actual Spending']
-    },
     radar: {
         // shape: 'circle',
         indicator: [
-            { name: 'Sales', max: 6500 },
-            { name: 'Administration', max: 16000 },
-            { name: 'Information Technology', max: 30000 },
-            { name: 'Customer Support', max: 38000 },
-            { name: 'Development', max: 52000 },
-            { name: 'Marketing', max: 25000 }
+            { name: 'Sales', max: 100 },
+            { name: 'Administration', max: 100 },
+            { name: 'Information Technology', max: 100 },
+            { name: 'Customer Support', max: 100 },
+            { name: 'Development', max:100 },
+            { name: 'Marketing', max: 100 },
         ]
     },
     series: [
@@ -23,12 +17,8 @@ var option = {
             type: 'radar',
             data: [
                 {
-                    value: [4200, 3000, 20000, 35000, 50000, 18000],
-                    name: 'Allocated Budget'
-                },
-                {
-                    value: [5000, 14000, 28000, 26000, 42000, 21000],
-                    name: 'Actual Spending'
+                    value: [80, 80, 80, 80, 80, 80],
+                    name: '技能雷达'
                 }
             ]
         }
