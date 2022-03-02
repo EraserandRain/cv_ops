79ab4acd7a5e448cac1ef807240286a4c97cfbc4
EraserandRain <1349291258@qq.com>
2022-03-02 09:43:54 +0800

    fix: css 404 bug

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ rm -rf dist/*
 rm -rf docs/*
 npx parcel build src/index.html --no-minify
 /usr/bin/cp -r dist/* docs/
-css=$(ls -l docs|grep "css$"|awk '{print $NF}')
-js=$(ls -l docs|grep "js$"|awk '{print $NF}')
-sed -i "s|/${css}|/cv/main/docs/${css}|" ./docs/index.html
-sed -i "s|/${js}|/cv/main/docs/${js}|" ./docs/index.html
+# css=$(ls -l docs|grep "css$"|awk '{print $NF}')
+# js=$(ls -l docs|grep "js$"|awk '{print $NF}')
+# sed -i "s|/${css}|/cv/main/docs/${css}|" ./docs/index.html
+# sed -i "s|/${js}|/cv/main/docs/${js}|" ./docs/index.html
 exit 0
