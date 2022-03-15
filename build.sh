d91ddb6f44a0f88fc0dd3736dcceb509259ad71e
EraserandRain <1349291258@qq.com>
2022-03-16 00:05:48 +0800

    perf: touch docs/.nojekyll

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ rm -rf dist/*
 rm -rf docs/*
 npx parcel build src/index.html --no-minify
 /usr/bin/cp -r dist/* docs/
-css=$(ls -l docs|grep "css$"|awk '{print $NF}')
-js=$(ls -l docs|grep "js$"|awk '{print $NF}')
-pic=$(ls -l docs|grep "jpg$"|awk '{print $NF}')
-sed -i "s|/${css}|./${css}|" docs/index.html
-sed -i "s|/${js}|./${js}|" docs/index.html
-sed -i "s|/${pic}|./${pic}|" docs/index.html
+# css=$(ls -l docs|grep "css$"|awk '{print $NF}')
+# js=$(ls -l docs|grep "js$"|awk '{print $NF}')
+# pic=$(ls -l docs|grep "jpg$"|awk '{print $NF}')
+# sed -i "s|/${css}|./${css}|" docs/index.html
+# sed -i "s|/${js}|./${js}|" docs/index.html
+# sed -i "s|/${pic}|./${pic}|" docs/index.html
+touch docs/.nojekyll
 exit 0
