dbe23ff7b5704fa6f10f5b062b8cf1b346257cd1
EraserandRain <1349291258@qq.com>
2024-03-22 13:08:22 +0800

    refactoring

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+
+import next from 'next'
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    output: 'export'
+}
+
+export default nextConfig
