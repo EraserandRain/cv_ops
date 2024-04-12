d2c758044e092fbd64b064e1541b45b16e010516
EraserandRain <1349291258@qq.com>
2024-04-12 15:08:07 +0800

    perf: Simplify the structure of SocialLink

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -12,30 +12,20 @@ interface SocialLinkProps {
 }
 
 const SocialLink = ({ text, desc, url, icon: Icon }: SocialLinkProps) => {
-  if (typeof desc === 'string') {
-    return (
-      <p className="flex items-center">
-        <span>
-          <Icon className='mt-2' />
-        </span>
-        <span>
-          【<a href={url}>{text}</a>】 : {desc}
-        </span>
-      </p>
-    )
-  } else {
-    const { mainStr, subStrs } = desc as { mainStr: string; subStrs: string[] }
-    return (
-      <p className="flex items-center">
-        <span>
-          <Icon className='mt-2' />
-        </span>
-        <span>
-          【<a href={url}>{text}</a>】 : <StrongText mainStr={mainStr} subStrs={subStrs} />
-        </span>
-      </p>
-    )
-  }
+  const { mainStr, subStrs } = desc as { mainStr: string; subStrs: string[] } || {}
+  return (
+    <p className="flex items-center">
+      <span>
+        <Icon className='mt-2' />
+      </span>
+      <span>
+        【<a href={url}>{text}</a>】 : {(typeof desc === 'string') ? desc : <StrongText
+          mainStr={mainStr}
+          subStrs={subStrs}
+        />}
+      </span>
+    </p>
+  )
 }
 
 export default SocialLink
